3437cba56107d0f63dad794658f0132224b8eb35
SoWieMarkus <wie.markus@web.de>
2021-04-08 23:39:29 +0200

    added create item activity

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,15 @@ w
 Gapp/src/main/java/markus/wieland/defaultappcomponents/MainActivity.java,e\3\e3dfca55e5d5131452569d18d5d952fd8257b8a0
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+ˆ
+XdefaultAppComponents/src/main/java/markus/wieland/defaultappelements/api/GetRequest.java,6\f\6f6c942cc0d969dd05135bddf76c273563d8fe4b
+Q
+!defaultAppComponents/build.gradle,0\5\0541710c298914ba2497161bcc6490685c1deaa5
+“
+cdefaultAppComponents/src/main/java/markus/wieland/defaultappelements/api/RequestResultListener.java,0\e\0e974e4814f93452dd9cc646919c0672fa544898
+Ÿ
+odefaultAppComponents/src/main/java/markus/wieland/defaultappelements/uielements/activities/DefaultActivity.java,d\f\df1153eeb4889279cc7f99cc226e88fd08db72ab
+ž
+ndefaultAppComponents/src/main/java/markus/wieland/defaultappelements/uielements/fragments/DefaultFragment.java,1\b\1b6dfa1b0f13bff3c115d1b3aa61b9c4e8909488
---
+++ defaultAppComponents/src/main/java/markus/wieland/defaultappelements/uielements/activities/CreateItemActivity.java
@@ -0,0 +1,82 @@
+package markus.wieland.defaultappelements.uielements.activities;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+
+import markus.wieland.defaultappelements.R;
+
+public abstract class CreateItemActivity<I extends Serializable> extends DefaultActivity {
+
+    public static final String OBJECT_TO_EDIT = "markus.wieland.defaultappcomponents.createitemactivity.OBJECT_TO_EDIT";
+    public static final String RESULT = "markus.wieland.defaultappcomponents.createitemactivity.RESULT";
+
+    public static final int REQUEST_EDIT = 1;
+    public static final int REQUEST_CREATE = 2;
+
+    protected I item;
+
+    public CreateItemActivity(int layout) {
+        super(layout);
+    }
+
+    @Override
+    protected void initialize() {
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_activity_create_item_close);
+        }
+
+        item = (I) getIntent().getSerializableExtra(OBJECT_TO_EDIT);
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+        if (item == null) initializeViewsAddMode();
+        else initializeViewsEditMode();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_create_item, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        } else if (item.getItemId() == R.id.menu_activity_create_item_commit) {
+            onCommitItem();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Will be executed if the instance which is passed with the intent is not null
+     */
+    public abstract void initializeViewsEditMode();
+
+    /**
+     * Will be executed if the instance which is passed with the intent is null
+     */
+    public abstract void initializeViewsAddMode();
+
+    protected abstract boolean validateItem();
+
+    protected abstract void onCommitItem();
+
+    public void commitItem() {
+        if (!validateItem()) return;
+        Intent data = new Intent();
+        data.putExtra(RESULT, item);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
